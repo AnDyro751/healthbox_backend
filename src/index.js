f6f170dd7f77d205eb4c30732513ce777d69d3ea
Angel Mendez <angelmendezz751@gmail.com>
2020-04-08 15:26:54 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ server.express.use(async (req, res, next) => {
 
 server.start({
     cors: {
-        origin: "http://localhost:3000"
+        origin: "http://localhost:3000",
+        credentials: false
     }
 }, () => console.log(`The server is running on http://localhost:4000`))
