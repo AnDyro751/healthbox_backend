9bb46fbe04d80429afcc2cfc7fd20d4115096e20
Angel Mendez <angelmendezz751@gmail.com>
2020-04-08 23:23:29 -0500

    prisma ge

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -36,6 +36,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466`
+  endpoint: `http://healthboxapi.azachii.tech:4466/`
 });
 exports.prisma = new exports.Prisma();
